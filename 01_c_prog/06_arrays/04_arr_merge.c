e5fc5421248a5c9771f123497c363498987ecbfa
Adarsh Kv <95900345+1adarshkv9@users.noreply.github.com>
2023-04-03 20:57:20 +0530

    Update 04_arr_merge.c

--- 01_c_prog/06_arrays/04_arr_merge.c
+++ 01_c_prog/06_arrays/04_arr_merge.c
@@ -8,8 +8,8 @@
  *
  * Application: 
  * Brief: 
- * Author: Natarajan S  ( natarajan.s@ruggedboard.com )
- * Title: Program Manager / Trainner
+ * Author: 
+ * Title: 
  * Last Modified Date: 19.09.2022
  *
 /*********************************************************************************************/
@@ -17,11 +17,45 @@
 // Header File
 #include <stdio.h>
 
-// Main Function
-int main()
-{
-     
-     return 0;
+int main() {
+    int size1, size2, size3, i, j;
+    printf("Enter the size of the first array: ");
+    scanf("%d", &size1);
+    int arr1[size1];
+    printf("Enter the elements of the first array:\n");
+    for(i = 0; i < size1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+    printf("\nfirst array:");
+    for(i = 0; i < size1; i++) {
+        printf("%d ", arr1[i]);
+    }
+    
+    printf("\nEnter the size of the second array: ");
+    scanf("%d", &size2);
+    int arr2[size2];
+    printf("Enter the elements of the second array:\n");
+    for(i = 0; i < size2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+    printf("second array:");
+    for(i = 0; i < size2; i++) {
+        printf("%d ", arr2[i]);
+    }
+    size3 = size1 + size2;
+    int arr3[size3];
+    for(i = 0; i < size1; i++) {
+        arr3[i] = arr1[i];
+    }
+    for(j = 0; j < size2; j++) {
+        arr3[i] = arr2[j];
+        i++;
+    }
+    printf("\nThe merged array:\n");
+    for(i = 0; i < size3; i++) {
+        printf("%d ", arr3[i]);
+    }
+    return 0;
 }
 
 // Program End
