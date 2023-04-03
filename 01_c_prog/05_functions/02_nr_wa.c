7331bfc82a449f2178eeadbaeec3496cd5c28a4c
Adarsh Kv <95900345+1adarshkv9@users.noreply.github.com>
2023-04-03 14:45:30 +0530

    Update 02_nr_wa.c

--- 01_c_prog/05_functions/02_nr_wa.c
+++ 01_c_prog/05_functions/02_nr_wa.c
@@ -8,8 +8,8 @@
  *
  * Application: 
  * Brief: 
- * Author: Natarajan S  ( natarajan.s@ruggedboard.com )
- * Title: Program Manager / Trainner
+ * Author: 
+ * Title:
  * Last Modified Date: 19.09.2022
  *
 /*********************************************************************************************/
