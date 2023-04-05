3b6ccef7aa52404b1091c715e0fd1da3101f630d
Adarsh Kv <95900345+1adarshkv9@users.noreply.github.com>
2023-04-05 20:43:16 +0530

    Update 02_poin_use.c

--- 01_c_prog/07_pointers/02_poin_use.c
+++ 01_c_prog/07_pointers/02_poin_use.c
@@ -8,8 +8,8 @@
  *
  * Application: 
  * Brief: 
- * Author: Natarajan S  ( natarajan.s@ruggedboard.com )
- * Title: Program Manager / Trainner
+ * Author: 
+ * Title: 
  * Last Modified Date: 19.09.2022
  *
 /*********************************************************************************************/
@@ -17,11 +17,46 @@
 // Header File
 #include <stdio.h>
 
-// Main Function
-int main()
-{
-     
-     return 0;
+int main() {
+    int m = 300;
+    float fx = 300.6;
+    char cht = 'z';
+
+    printf("Pointer : Demonstrate the use of & and * operator :\n");
+    printf("--------------------------------------------------------\n");
+    printf(" m = %d\n", m);
+    printf(" fx = %f\n", fx);
+    printf(" cht = %c\n", cht);
+
+    printf("\n Using & operator :\n");
+    printf("-----------------------\n");
+    printf(" address of m = %p\n", &m);
+    printf(" address of fx = %p\n", &fx);
+    printf(" address of cht = %p\n", &cht);
+
+    printf("\n Using & and * operator :\n");
+    printf("----------------------------\n");
+    printf(" value at address of m = %d\n", *(&m));
+    printf(" value at address of fx = %f\n", *(&fx));
+    printf(" value at address of cht = %c\n", *(&cht));
+
+    int *m_ptr = &m;
+    float *fx_ptr = &fx;
+    char *cht_ptr = &cht;
+
+    printf("\n Using only pointer variable :\n");
+    printf("----------------------------------\n");
+    printf(" address of m = %p\n", m_ptr);
+    printf(" address of fx = %p\n", fx_ptr);
+    printf(" address of cht = %p\n", cht_ptr);
+
+    printf("\n Using only pointer operator :\n");
+    printf("----------------------------------\n");
+    printf(" value at address of m = %d\n", *m_ptr);
+    printf(" value at address of fx= %f\n", *fx_ptr);
+    printf(" value at address of cht= %c\n", *cht_ptr);
+
+    return 0;
 }
 
 // Program End
