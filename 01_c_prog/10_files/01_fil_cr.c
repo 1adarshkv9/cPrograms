f0ca26bd0dafff8c1fe8daba75eefff4d89f55ff
Adarsh Kv <95900345+1adarshkv9@users.noreply.github.com>
2023-04-10 19:47:31 +0530

    Update 01_fil_cr.c

--- 01_c_prog/10_files/01_fil_cr.c
+++ 01_c_prog/10_files/01_fil_cr.c
@@ -1,27 +1,28 @@
-/*********************************************************************************************
- * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
- * this source code, object code, or executable code (collectively, Software), is granted only
- * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
- * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
- * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
- * For further information, contact community.ruggedboard.com / info@ruggedboard.com
- *
- * Application: 
- * Brief: 
- * Author: Natarajan S  ( natarajan.s@ruggedboard.com )
- * Title: Program Manager / Trainner
- * Last Modified Date: 19.09.2022
- *
-/*********************************************************************************************/
-
-// Header File
 #include <stdio.h>
 
-// Main Function
-int main()
-{
-     
-     return 0;
-}
+int main() {
+    FILE *fptr;
+    char filename[100], c;
 
-// Program End
+    
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+
+    
+    fptr = fopen(filename, "r");
+    if (fptr == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    
+    printf("File contents:\n");
+    while ((c = fgetc(fptr)) != EOF) {
+        printf("%c", c);
+    }
+
+
+    fclose(fptr);
+
+    return 0;
+}
